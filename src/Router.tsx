f6df8639afe8960628424decfa5ba17eab883ec3
pobiq <hoon2207@naver.com>
2025-12-10 09:51:02 +0900

    vercel 자동배포 테스트

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,14 @@ import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import TestPage from "./pages/TestPage";
 import RootLayout from "./layouts/RootLayout";
+import DeployPage from "./pages/DeployPage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/deploy" element={<DeployPage />} />
         <Route path="/login/oauth2/callback" element={<LoginPage />} />
         <Route element={<RootLayout />}>
           <Route path="/main" element={<MainPage />} />
---
+++ src/pages/DeployPage.tsx
@@ -0,0 +1,7 @@
+export default function DeployPage() {
+  return (
+    <>
+      <h1>배포 확인용</h1>
+    </>
+  );
+}
